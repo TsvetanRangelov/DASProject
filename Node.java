642a42f87858cbe79eb5156dd4f8fc8faa28f0d1
Nikolas Pitsillos <npitsillos@hotmail.com>
2017-11-19 14:58:12 +0000

    Cleaned repo

--- Node.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.RemoteException;
-
-public interface Node extends java.rmi.Remote {
-
-    public String startElection(String senderName) throws RemoteException;
-    public void leader(String leaderName) throws RemoteException;
-    //public String receiveMsg(String message);
-}
-
